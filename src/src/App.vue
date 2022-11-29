b4a861b83adc321a5bfd96c4ce4c688383290526
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2022-11-29 21:21:10 +0900

    [fix] 既存ファイルをcompositionApiへ置き換え

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,24 +1,33 @@
 <template>
-  <img src="./assets/logo.png">
-  <div>
-    <p>
-      If Element Plus is successfully added to this project, you'll see an
-      <code v-text="'<el-button>'"></code>
-      below
-    </p>
-    <el-button type="primary">el-button</el-button>
-  </div>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+    <h2>Read Recode</h2>
+
+    <el-menu mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+        <el-menu-item index="1">
+            <router-link to="/">TopHome</router-link>
+        </el-menu-item>
+        <el-menu-item index="2">
+            <router-link to="/search">Search</router-link>
+        </el-menu-item>
+        <el-submenu index="3">
+            <!-- #はv-slotの省略記法 -->
+            <template #title>Support</template>
+            <el-menu-item index="3-1">
+                <a href="#" target="heip">Author</a>
+            </el-menu-item>
+            <el-menu-item index="3-2">
+                <a href="#" target="heip">Publisher</a>
+            </el-menu-item>
+        </el-submenu>
+    </el-menu>
+
+    <router-view />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
